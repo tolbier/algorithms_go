302dbfd8b1c6e0a2d46d905e7254a183ee4dd3f8
Lluis Toyos <lluis.toyos@cabify.com>
2020-08-16 14:08:43 +0200

    Refactored Graph Lib

--- lib/graphs/edge.go
+++ lib/graphs/edge.go
@@ -1,13 +1,16 @@
 package graphs
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 type Edges []*Edge
-type EdgeType bool
+type Directed bool
 type Edge struct {
 	vertex1    *Vertex
 	vertex2    *Vertex
-	isDirected EdgeType
+	isDirected Directed
 	weight     float64
 }
 
@@ -17,7 +20,7 @@ func NewEdge(vertex1, vertex2 *Vertex, isDirected bool, options ...EdgeOption) (
 	edge = &Edge{
 		vertex1:    vertex1,
 		vertex2:    vertex2,
-		isDirected: EdgeType(isDirected),
+		isDirected: Directed(isDirected),
 	}
 	for _, option := range options {
 		edge = option(edge)
@@ -32,27 +35,27 @@ func SetWeight(weight float64) EdgeOption {
 	}
 }
 
-func (e *Edge) GetVertex1() *Vertex {
+func (e Edge) GetVertex1() *Vertex {
 	return e.vertex1
 }
-func (e *Edge) GetVertex2() *Vertex {
+func (e Edge) GetVertex2() *Vertex {
 	return e.vertex2
 }
-func (e *Edge) IsDirected() bool {
+func (e Edge) IsDirected() bool {
 	return bool(e.isDirected)
 }
 
-func (e *Edge) IsUndirected() bool {
+func (e Edge) IsUndirected() bool {
 	return !bool(e.isDirected)
 }
 
-func (e *Edge) Weight() float64 {
+func (e Edge) Weight() float64 {
 	return e.weight
 }
-func (e *Edge) String() string {
+func (e Edge) String() string {
 	return fmt.Sprintf("{%d:%d:%.2f:%s}", e.vertex1.ID, e.vertex2.ID, e.weight, e.isDirected)
 }
-func (et EdgeType) String() string {
+func (et Directed) String() string {
 	if et {
 		return "D"
 	}
@@ -62,3 +65,16 @@ func (ee Edges) String() string {
 	res := fmt.Sprintf("%v", []*Edge(ee))
 	return res
 }
+
+func (ee Edges) sorted() (sortedEdges Edges) {
+	sortedEdges = make(Edges, len(ee))
+	copy(sortedEdges, ee)
+	sort.Sort(sortedEdges)
+	return
+}
+func (ee Edges) Len() int { return len(ee) }
+func (ee Edges) Less(i, j int) bool {
+	return ee[i].vertex1.ID < ee[j].vertex1.ID ||
+		(ee[i].vertex1.ID == ee[j].vertex1.ID && ee[i].vertex2.ID < ee[j].vertex2.ID)
+}
+func (ee Edges) Swap(i, j int) { ee[i], ee[j] = ee[j], ee[i] }
--- lib/graphs/vertex.go
+++ lib/graphs/vertex.go
@@ -5,11 +5,13 @@ import (
 	"sort"
 )
 
+type VerticesMap map[int]*Vertex
+type Vertices []*Vertex
 type Vertex struct {
 	ID               int
 	Value            interface{}
 	inEdges, edges   Edges
-	adjacentVertices []*Vertex
+	adjacentVertices Vertices
 }
 
 func NewVertex(ID int) *Vertex {
@@ -21,7 +23,7 @@ func NewVertexWithValue(ID int, Value interface{}) *Vertex {
 		Value:            Value,
 		inEdges:          Edges{},
 		edges:            Edges{},
-		adjacentVertices: []*Vertex{},
+		adjacentVertices: Vertices{},
 	}
 }
 
@@ -34,7 +36,7 @@ func (v *Vertex) AddAdjacentVertex(edge *Edge, adjVertex *Vertex) {
 func (v *Vertex) addInEdge(edge *Edge) {
 	v.inEdges = append(v.inEdges, edge)
 }
-func (v *Vertex) GetAdjacentVertices() []*Vertex {
+func (v *Vertex) GetAdjacentVertices() Vertices {
 	return v.adjacentVertices
 }
 func (v *Vertex) GetAdjacentIDs() (res []int) {
@@ -48,3 +50,28 @@ func (v *Vertex) GetAdjacentIDs() (res []int) {
 func (v *Vertex) String() string {
 	return fmt.Sprintf("{%d(%v):%v}", v.ID, v.Value, v.GetAdjacentIDs())
 }
+func (vm VerticesMap) getSortedVertices() (vertices Vertices) {
+	vertices = make(Vertices, 0, len(vm))
+
+	for _, vertex := range vm {
+		vertices = append(vertices, vertex)
+	}
+	sort.Sort(vertices)
+	return
+}
+func (vm VerticesMap) getVertices(id1, id2 int) (v1, v2 *Vertex) {
+	return vm.getVertex(id1), vm.getVertex(id2)
+}
+func (vm VerticesMap) getVertex(id int) (v *Vertex) {
+	var ok bool
+	if v, ok = vm[id]; !ok {
+		v = NewVertex(id)
+		vm[v.ID] = v
+	}
+	return v
+}
+func (vv Vertices) Len() int { return len(vv) }
+func (vv Vertices) Less(i, j int) bool {
+	return vv[i].ID < vv[j].ID
+}
+func (vv Vertices) Swap(i, j int) { vv[i], vv[j] = vv[j], vv[i] }
